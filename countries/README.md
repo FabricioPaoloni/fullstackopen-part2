95a2506d7c63ce6ff45326fa9ae05845ac854d5f
Fabricio Paoloni <paolonifabricio@gmail.com>
2025-01-12 22:39:44 -0300

    Exercise 2-20 completed: Data for countries, step 3. Weather information added to the project. You need an API key to run the app as explained in the course. https://fullstackopen.com/en/part2/adding_styles_to_react_app\#exercises-2-18-2-20

--- countries/README.md
+++ countries/README.md
@@ -1,3 +1,16 @@
+You need an [openweathermap.org](https://openweathermap.org/) API key to run this project. This project use that API key to consult weather information. To run the app run the following command: 
+
+Assuming the api-key is 54l41n3n4v41m34rv0, when the application is started like so:
+
+export VITE_SOME_KEY=54l41n3n4v41m34rv0 && npm run dev // For Linux/macOS Bash
+($env:VITE_SOME_KEY="54l41n3n4v41m34rv0") -and (npm run dev) // For Windows PowerShell
+set "VITE_SOME_KEY=54l41n3n4v41m34rv0" && npm run dev // For Windows cmd.exe
+
+
+
+---------------------------------------------------------------------------
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- countries/src/components/Country.jsx
+++ countries/src/components/Country.jsx
@@ -1,20 +1,63 @@
-const Country = ({country}) => {
+const api_key = import.meta.env.VITE_SOME_KEY
+import { useState ,useEffect } from "react"
+import axios from 'axios'
+
+const Country = ({ country }) => {
+    const [countryWeather, setCountryWeather] = useState({})
+
     let langArray = []
     for (const language in country.languages) {
         // console.log(country.languages[language])
-        langArray.push(<li key={country.languages.language}>{country.languages[language]}</li>)
+        langArray.push(country.languages[language])
     }
+
     // console.log(langArray)
-    return(
+
+
+    useEffect(() => {
+        if(country.capital) {
+
+            axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${api_key}`)
+            .then(response => {
+                setCountryWeather(response.data)
+                // console.log(response.data)
+            })
+            .catch(error => {
+                console.log('An error ocurred importing weather info.')
+            })
+        }
+    }, [country])
+    // console.log(countryWeather)
+
+    return (
         <div>
             <h2>{country.name.common}</h2>
             <p><b>Capital:</b> {country.capital ? country.capital[0] : null}</p>
             <p><b>Area:</b> {country.area ? country.area.toLocaleString() : null} km<sup>2</sup></p>
             <p><b>Population:</b> {country.population ? country.population.toLocaleString() : null} citizens</p>
             <ul>
-                {langArray}
+                {langArray.map(language => {
+                    return <li key={language}>{language}</li>
+                })}
             </ul>
             <img src={country.flags.png ? country.flags.png : null} alt="" />
+
+            {
+                !country.capital ?
+                <p>Capital country data is missing :/</p> :
+                Object.keys(countryWeather).length === 0 ?
+                    <p>Importing weather data... {}</p> :
+                    <div>
+                        <h3>Weather in {country.capital[0]}:</h3>
+                        <p><b>Temperature:</b> {(countryWeather.main.temp - 273.15).toFixed(2)} ºC</p>
+                        <img src={`https://openweathermap.org/img/wn/${countryWeather.weather[0].icon}@2x.png`} type='png' alt='logo del clima'></img>
+                        <p><b>Wind:</b> {countryWeather.wind.speed} m/s</p>
+                        <p><b>Humidity:</b> {countryWeather.main.humidity}%</p>
+                    </div>
+
+            }
+
         </div>
     )
 }
